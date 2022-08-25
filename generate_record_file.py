86420fea945c7c8bd0ea8924ea3ff6c7a21168b9
jie <f5666889092@gmail.com>
2022-08-25 16:21:06 +0800

    update

--- generate_record_file.py
+++ generate_record_file.py
@@ -1,3 +1,4 @@
+# 生成每個學者的紀錄檔
 from pymongo import MongoClient
 from module.save_to_txt import save_to_txt
 from module.remove_exist_file import remove_exist_file
@@ -18,13 +19,11 @@ def generate_record_file(read_data_withID_file, filename):
             scholar_profile = list(db.cguscholar.find({"_id":data[0]}))
 
             if(scholar_profile):
-
                 data.append(len(scholar_profile[0]['citedRecord']))
                 for record in scholar_profile[0]['citedRecord']:
                     updateTime = record['updateTime'].replace("-", "").split()[0] # 2022-05-15 14:20:09 => 20220515
                     citationCount = record['cited']['citations']['All'] if record['cited'] else 0
                     data.extend([updateTime, citationCount])
-            print(data)
             recordList.append(data)
         if(len(recordList) > 0): save_to_txt(filename, recordList)
 
