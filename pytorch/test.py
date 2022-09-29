fc43844bff61cfcc51c45dcffa16e546c134ea80
jie <f5666889092@gmail.com>
2022-09-29 10:01:02 +0800

    update

--- pytorch/test.py
+++ pytorch/test.py
@@ -1,18 +1,21 @@
-from itertools import count
 import numpy as np
 import subprocess
 import time
 import torch
-from sklearn.model_selection import StratifiedKFold
-from module.save_to_txt import save_to_txt
+from   sklearn.model_selection import StratifiedKFold
+from   sklearn.preprocessing import MinMaxScaler
+from   module.save_to_txt import save_to_txt
 
 # config
-batch_size = 64
-input_features = 1000   # the number of expected features in the input x
-hidden_feature_dim = 128 # the number of features in the hidden state
-model_layer_num = 4     # number of recurrent layers
-output_dim = 2          # model's output
+batch_size = 32
+input_features = 1300   # the number of expected features in the input x
+hidden_feature_dim = 512 # the number of features in the hidden state
+model_layer_num = 8     # number of recurrent layers
+output_dim = 1          # model's output
 epochs_num = 10
+kFolds_num = 5
+learningRate = 0.001
+
 # 設定訓練檔與測試檔
 trainFile= "./random_balance_dupli.txt"
 testFile = "./random_balance.txt"
@@ -31,7 +34,7 @@ class MyDataset(torch.utils.data.Dataset):
     def __len__(self):
         return self.data_tensor.size(0)
 
-# 創建iterable dateset
+# Create Iterable dataset
 class MyIterableDataset(torch.utils.data.IterableDataset):
     def __init__(self, arg_list):
         self.filePath = arg_list["path"]
@@ -51,24 +54,33 @@ class MyIterableDataset(torch.utils.data.IterableDataset):
                     data.extend([0]*(self.each_scholar_vectorLen - len(data) + 2))
                 # 取 data[2]之後的值 (data[0] : label, data[1] : ID)
                 train = [data[2:]]
-
                 train = np.array(train)
                 label = np.array(label)
                 train = train.astype("float32")
+                # 正規化
+                scaler = MinMaxScaler(feature_range=(0, 1))
+                train_scaled = scaler.fit_transform(train.reshape(self.each_scholar_vectorLen, -1))
+                train = train_scaled.reshape(-1, self.each_scholar_vectorLen)
+
                 #print(f"{data[1]} train.shape: {train.shape}, label.shape: {label.shape} ")
                 train = torch.tensor(train)
                 label = torch.tensor(label)
                 yield(train, label)
 
-
 class BiLSTM(torch.nn.Module):
     def __init__(self):
         super(BiLSTM, self).__init__()
 
-        self.lstm = torch.nn.LSTM(input_size = input_features, hidden_size =  hidden_feature_dim, num_layers = model_layer_num,
-                            bidirectional=True, bias = False) # input, output: (seq, batch_size, feature)
+        self.lstm = torch.nn.LSTM(input_size = input_features,
+                                  hidden_size =  hidden_feature_dim,
+                                  num_layers = model_layer_num,
+                                  bidirectional=True)
+                                  #bias = False) # input, output: (seq, batch_size, feature)
         # fully connected layer
         self.fc =torch.nn.Linear(hidden_feature_dim*2, output_dim)
+
+        self.sigmoid = torch.nn.Sigmoid()
+        self.tanh = torch.nn.Tanh()
 
     def forward(self, X):
         # X: [batch_size, seq, feature]
@@ -80,15 +92,19 @@ class BiLSTM(torch.nn.Module):
 
         model_out, (h_n, c_n) = self.lstm(input, (hidden_state, cell_state))
 
-        output = self.fc(model_out[-1,:,:])
-        softmax = torch.nn.Softmax(dim=1)
-        output = softmax(output)
-        return output
+        out = self.fc(model_out[-1,:,:])
+        out = self.tanh(out)
+        out = self.sigmoid(out)
 
+        out = out.view(batch_size, -1)
+        out = out[:, -1]
+        return out
+
+# 計算準確率
 def count_accuracy(outputs, labels):
     count = 0
     for out, lab in zip(outputs, labels):
-        if (out[0] > out[1] and lab == 0) or (out[0] < out[1] and lab == 1):
+        if (out >= 0.5 and lab == 1) or (out < 0.5 and lab == 0):
             count += 1
     return count
 
@@ -96,18 +112,15 @@ def train_model(model, epochs_num, batch_size, criterion, optimizer):
     model.train()
     for epoch in range(epochs_num):
         train_set = MyIterableDataset(train_arg_list)
-        loader = torch.utils.data.DataLoader(train_set, batch_size = batch_size, shuffle=False)
+        loader = torch.utils.data.DataLoader(train_set, batch_size = batch_size)
         accuracy = 0
         loss_epoch = 0
         for data, label in loader:
-
             model_out = model(data)
-            #label = label.type(torch.LongTensor)
-
             accuracy += count_accuracy(model_out, label)
 
             # 計算loss
-            loss = criterion(model_out, label)
+            loss = criterion(model_out, label.float())
             loss_epoch += loss
 
             optimizer.zero_grad() # 將梯度歸0
@@ -119,21 +132,18 @@ def train_model(model, epochs_num, batch_size, criterion, optimizer):
         loss_epoch = loss+epoch/int(trainset_num)
         print(f"Eopch {epoch+1}/{epochs_num}, Loss: { '{:.3f}'.format(loss_epoch) }, Accuracy: {accuracy_epoch}%")
 
-def test_model(model):
+def test_model(model, criterion):
     model.eval()
     accuracy = 0
     loss = 0
     testset = MyIterableDataset(test_arg_list)
-    loader = torch.utils.data.DataLoader(testset, batch_size = batch_size, shuffle=False)
+    loader = torch.utils.data.DataLoader(testset, batch_size = batch_size)
     with torch.no_grad():
         for data, label in loader:
             model_out = model(data)
-            label = label.type(torch.LongTensor)
-
             accuracy += count_accuracy(model_out, label)
-
             # 計算loss
-            loss = criterion(model_out, label)
+            loss = criterion(model_out, label.float())
 
     loss = loss/int(testset_num)
     accuracy = round(accuracy*100/int(testset_num), 3)
@@ -142,10 +152,9 @@ def test_model(model):
 start_time = time.time()
 
 model = BiLSTM()
-#criterion = torch.nn.MSELoss() # mean squared error : input x and target y.
-criterion = torch.nn.CrossEntropyLoss() # mean squared error : input x and target y.
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001) #0.01-0.001 lr = 0.001
-
+criterion = torch.nn.BCELoss() # mean squared error : input x and target y.
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.001) #0.01-0.001 lr = 0.001
+optimizer = torch.optim.SGD(model.parameters(), lr= learningRate) #0.01-0.001 lr = 0.001
 train_model(model, epochs_num, batch_size, criterion, optimizer)
 execute = (time.time() - start_time)
 print("train model : ",time.strftime("%H:%M:%S", time.gmtime(execute)))
--- pytorch/test_kfold.py
+++ pytorch/test_kfold.py
@@ -2,21 +2,23 @@ import numpy as np
 import subprocess
 import time
 import torch
-from sklearn.model_selection import StratifiedKFold
-from module.save_to_txt import save_to_txt
+from   sklearn.model_selection import StratifiedKFold
+from   sklearn.preprocessing import MinMaxScaler
+from   module.save_to_txt import save_to_txt
 
 # config
-batch_size = 64
-input_features = 1000   # the number of expected features in the input x
+batch_size = 32
+input_features = 800   # the number of expected features in the input x
 hidden_feature_dim = 512 # the number of features in the hidden state
-model_layer_num = 16     # number of recurrent layers
-output_dim = 2          # model's output
+model_layer_num = 8     # number of recurrent layers
+output_dim = 1          # model's output
 epochs_num = 10
 kFolds_num = 5
-learningRate = 0.08
+learningRate = 0.01
+
 # 設定訓練檔與測試檔
 trainFile= "./random_balance_dupli.txt"
-testFile = "./random_balance.txt"
+testFile = "./random_balance_dupli.txt"
 trainset_num = subprocess.getstatusoutput(f"wc -l {trainFile}")[1].split()[0]
 testset_num = subprocess.getstatusoutput(f"wc -l {testFile}")[1].split()[0]
 print(f"train: {trainset_num}, test: {testset_num} ")
@@ -52,10 +54,14 @@ class MyIterableDataset(torch.utils.data.IterableDataset):
                     data.extend([0]*(self.each_scholar_vectorLen - len(data) + 2))
                 # 取 data[2]之後的值 (data[0] : label, data[1] : ID)
                 train = [data[2:]]
-
                 train = np.array(train)
                 label = np.array(label)
                 train = train.astype("float32")
+                # 正規化
+                scaler = MinMaxScaler(feature_range=(0, 1))
+                train_scaled = scaler.fit_transform(train.reshape(self.each_scholar_vectorLen, -1))
+                train = train_scaled.reshape(-1, self.each_scholar_vectorLen)
+
                 #print(f"{data[1]} train.shape: {train.shape}, label.shape: {label.shape} ")
                 train = torch.tensor(train)
                 label = torch.tensor(label)
@@ -65,10 +71,16 @@ class BiLSTM(torch.nn.Module):
     def __init__(self):
         super(BiLSTM, self).__init__()
 
-        self.lstm = torch.nn.LSTM(input_size = input_features, hidden_size =  hidden_feature_dim, num_layers = model_layer_num,
-                            bidirectional=True, bias = False) # input, output: (seq, batch_size, feature)
+        self.lstm = torch.nn.LSTM(input_size = input_features,
+                                  hidden_size =  hidden_feature_dim,
+                                  num_layers = model_layer_num,
+                                  bidirectional=True)
+                                  #bias = False) # input, output: (seq, batch_size, feature)
         # fully connected layer
         self.fc =torch.nn.Linear(hidden_feature_dim*2, output_dim)
+
+        self.sigmoid = torch.nn.Sigmoid()
+        self.tanh = torch.nn.Tanh()
 
     def forward(self, X):
         # X: [batch_size, seq, feature]
@@ -80,27 +92,30 @@ class BiLSTM(torch.nn.Module):
 
         model_out, (h_n, c_n) = self.lstm(input, (hidden_state, cell_state))
 
-        output = self.fc(model_out[-1,:,:])
-        softmax = torch.nn.Softmax(dim=1)
-        output = softmax(output)
-        return output
+        out = self.fc(model_out[-1,:,:])
+        out = self.tanh(out)
+        out = self.sigmoid(out)
 
+        out = out.view(batch_size, -1)
+        out = out[:, -1]
+        return out
+
+# 計算準確率
 def count_accuracy(outputs, labels):
     count = 0
     for out, lab in zip(outputs, labels):
-        if (out[0] > out[1] and lab == 0) or (out[0] < out[1] and lab == 1):
+        if (out >= 0.5 and lab == 1) or (out < 0.5 and lab == 0):
             count += 1
     return count
 
-def train_model(model, kFolds_num, epochs_num, batch_size, criterion):
-    optimizer = torch.optim.Adam(model.parameters(), lr=learningRate)
+def train_model(model, epochs_num, batch_size, criterion, optimizer):
     # Defind the k-fold cross validator
     kFold = StratifiedKFold(n_splits = kFolds_num, random_state=202, shuffle=True)
 
     for epoch in range(epochs_num):
         # 批次從檔案中拿出一部分的資料作為dataset
         dataset = MyIterableDataset(train_arg_list)
-        loader = torch.utils.data.DataLoader(dataset, batch_size = 4000, shuffle=False)
+        loader = torch.utils.data.DataLoader(dataset, batch_size = 3000, shuffle=False)
 
         for data_subset, label_subset in loader:
             data_label_subset = MyDataset(data_subset, label_subset)
@@ -115,8 +130,8 @@ def train_model(model, kFolds_num, epochs_num, batch_size, criterion):
                 valid_subsampler = torch.utils.data.SubsetRandomSampler(valid_indexs)
 
                 # Define data loaders for training and validating data in this fold
-                trainLoader = torch.utils.data.DataLoader(data_label_subset, batch_size = batch_size, sampler=train_subsampler, shuffle=False)
-                validLoader = torch.utils.data.DataLoader(data_label_subset, batch_size = batch_size, sampler=valid_subsampler, shuffle=False)
+                trainLoader = torch.utils.data.DataLoader(data_label_subset, batch_size = batch_size, sampler=train_subsampler)#, shuffle=False)
+                validLoader = torch.utils.data.DataLoader(data_label_subset, batch_size = batch_size, sampler=valid_subsampler) #shuffle=False)
 
                 # Set loss, accuracy value
                 loss_fold     = 0
@@ -130,9 +145,9 @@ def train_model(model, kFolds_num, epochs_num, batch_size, criterion):
                     model.train()
                     model_out = model(inputs)
 
-                    accuracy_fold += count_accuracy(model_out, labels)
+                    accuracy_fold += count_accuracy(model_out, labels.float())
 
-                    loss = criterion(model_out, labels)
+                    loss = criterion(model_out, labels.float())
                     loss_fold += loss
 
                     loss.backward() # 反向傳播計算每個參數的梯度值
@@ -144,17 +159,13 @@ def train_model(model, kFolds_num, epochs_num, batch_size, criterion):
 
                 print(f"fold {fold+1}/{kFolds_num}, Loss: { '{:.3f}'.format(loss_fold) }, Accuracy: {accuracy_fold}%")
 
-                # save model
-                #save_path = f"./model-fold-{fold}.pth"
-                #torch.save(model.state_dict(), save_path)
-
                 # validation theis fold
                 with torch.no_grad():
                     correct = 0
                     for valid_inputs, label in validLoader:
                         model_out = model(valid_inputs)
 
-                        correct += count_accuracy(model_out, label)
+                        correct += count_accuracy(model_out, label.float())
                     all_fold_accuracy[fold] = 100.0 * (correct/len(valid_indexs))
             print(f"********\nKFold cross validation results {kFolds_num} folds")
             sum = 0.0
@@ -165,30 +176,28 @@ def train_model(model, kFolds_num, epochs_num, batch_size, criterion):
 
 def test_model(model, criterion):
     model.eval()
-
     accuracy = 0
     loss = 0
     testset = MyIterableDataset(test_arg_list)
-    loader = torch.utils.data.DataLoader(testset, batch_size = batch_size, shuffle=False)
+    loader = torch.utils.data.DataLoader(testset, batch_size = batch_size)
     with torch.no_grad():
         for data, label in loader:
             model_out = model(data)
-            label = label.type(torch.LongTensor)
-
             accuracy += count_accuracy(model_out, label)
-
             # 計算loss
-            loss += criterion(model_out, label)
+            loss = criterion(model_out, label.float())
 
     loss = loss/int(testset_num)
     accuracy = round(accuracy*100/int(testset_num), 3)
     print(f"test\nLoss: { '{:.3f}'.format(loss) }, Accuracy: {accuracy}%")
 
-model = BiLSTM()
-criterion = torch.nn.CrossEntropyLoss()
-
 start_time = time.time()
-train_model(model, kFolds_num, epochs_num, batch_size, criterion)
+
+model = BiLSTM()
+criterion = torch.nn.BCELoss() # mean squared error : input x and target y.
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.001) #0.01-0.001 lr = 0.001
+optimizer = torch.optim.SGD(model.parameters(), lr=learningRate) #0.01-0.001 lr = 0.001
+train_model(model, epochs_num, batch_size, criterion, optimizer)
 execute = (time.time() - start_time)
 print("train model : ",time.strftime("%H:%M:%S", time.gmtime(execute)))
 
