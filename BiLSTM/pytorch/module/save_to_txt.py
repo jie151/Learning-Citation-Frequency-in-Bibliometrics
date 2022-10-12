2154fe1be5221a60bb77da5e8faebf47c2393e4a
jie <f5666889092@gmail.com>
2022-10-12 14:42:24 +0800

    del keras

--- BiLSTM/pytorch/module/save_to_txt.py
+++
@@ -1,6 +0,0 @@
-def save_to_txt(filename, dataList):
-    with open(filename, 'a', encoding = 'utf-8') as f:
-        for data in dataList:
-            for _data in data :
-                f.write("%s " % _data)
-            f.write("\n")
